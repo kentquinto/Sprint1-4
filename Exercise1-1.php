bd70f60b2ee78ff519b9e797cdb7939fcc80d565
Kent Quinto <kentquinto1@gmail.com>
2026-03-07 19:10:28 +0100

    update changes

--- Exercise1-1.php
+++ Exercise1-1.php
@@ -3,19 +3,22 @@ class employee {
     public $name;
     public $salary;
 
-function set_details($name, $salary){
-    $this->name = $name;
-    $this->salary = $salary;
-}
-function get_details() {
-    echo "Name: " . $this->name . " Salary: " . $this->salary . "€" . "\n";
-    if ($this->salary > 6000) {
-        echo "You need to pay taxes my friend!";
+    function data ($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+
+    }
+    function print() {
+        echo "Name: " . $this->name . "\n" .
+        "Salary: " . $this->salary . "€" . 
+        "\n";
+        if ($this->salary > 6000) {
+            echo "You need to pay taxes my friend!";
+        }
     }
 }
-}
 $employee1 = new employee();
+$employee1->data("Superman", 7000);
+$employee1->print();
 
-$employee1->set_details("Superman", 7000);
-$employee1->get_details();
 ?>
--- Exercise2-1.php
+++ Exercise2-1.php
@@ -7,27 +7,26 @@ class PokerDice {
         4 => "Jack",
         5 => "7",
         6 => "8"
-    ]
-    ;
+    ];
     public $lastRoll = [];
     public $totalRolls = 0;
 
     public function rollOne() {
-        $dieValue = rand(1, 6);
-        $result = $this->dice[$dieValue];
+        $randomNumber = rand(1, 6);
+        $result = $this->dice[$randomNumber];
         $this->lastRoll = [$result];
-        $this->totalRolls++;
+        $this->totalRolls = $this->totalRolls + 1;
         return $result;
     }
     public function roll($numberDice = 5) {
         $results = [];
         $this->lastRoll = [];
         for ($i = 0; $i < $numberDice; $i++) {
-            $dieValue = rand(1, 6);
-            $result = $this->dice[$dieValue];
+            $randomNumber = rand(1, 6);
+            $result = $this->dice[$randomNumber];
             $results[] = $result;
             $this->lastRoll[] = $result;
-            $this->totalRolls++;
+            $this->totalRolls = $this->totalRolls + 1;
         }
         return $results;
 
@@ -40,8 +39,12 @@ class PokerDice {
     }
 }
 $pokerDice = new PokerDice();
-echo "Rolling one die: " . $pokerDice->rollOne() . "\n";
-echo "Rolling five dice: " . implode(", ", $pokerDice->roll()) . "\n";
-echo "Last roll: " . implode(", ", $pokerDice->getLastRoll()) . "\n";
-echo "Total rolls: " . $pokerDice->getTotalRolls() . "\n";
+$oneRoll = $pokerDice->rollOne();
+echo "One roll: " . $oneRoll . "\n";
+$fiveRolls = $pokerDice->roll();
+echo "Five rolls: " . implode(", ", $fiveRolls) . "\n";
+$lastRoll = $pokerDice->getLastRoll();
+echo "Last roll: " . implode(", ", $lastRoll) . "\n";
+$totalRolls = $pokerDice->getTotalRolls();
+echo "Total rolls: " . $totalRolls . "\n";
 ?>
